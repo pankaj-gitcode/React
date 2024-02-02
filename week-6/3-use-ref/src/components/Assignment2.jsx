3e1ace1dfb2fd181049c5349a925b4a591cc74ea
pankaj-gitcode <pankajaditya75@gmail.com>
2024-02-03 05:02:04 +0530

    w6_3asignmt3_useRef_with_value0_vrsn2

--- week-6/3-use-ref/src/components/Assignment2.jsx
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -2,17 +2,18 @@
 // Use useRef to create a variable that persists across renders without causing additional renders when it changes.
 
 import React, {useRef, useState} from 'react'
-let reRenderTimes = 0;
+
 export function Assignment2(){
     const [count, setCount] = useState(0);
-
+    const reRenderTimes = useRef(0); //when page loads @ intial render the value starts from 1 not 2
     const clickHandle = ()=>{
         setCount(count=>count+1);
     }
-    reRenderTimes += 1;
+    reRenderTimes.current += 1;
     return(
         <div>
-            <p>This component has rendered {count} {reRenderTimes} times</p>
+            {/* reRenderTImes = 2times @ 1st render/pageload due to <React.StrictMode> */}
+            <p>This component has rendered {count} {reRenderTimes.current} times</p> 
             <button onClick={clickHandle}>Force Re-render</button>
         </div>
     )
