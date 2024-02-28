f89fb4bbffe4d7915931b3e6663c06fbb2991571
pankaj-gitcode <pankajaditya75@gmail.com>
2024-02-28 17:00:11 +0530

    recap_useEffect_vs_synchronus_code_rendering_time

--- revise/code_recap/src/App.jsx
+++ revise/code_recap/src/App.jsx
@@ -1,41 +1,14 @@
-// use of useMemo()
-// If I ask you to create an app that does two things -
-// 1. Increases a counter by 1
-// 2. Lets user put a value in an input box (n) and you need
-// to show sum from 1-n
-// One restriction - everything needs to be inside App()
-
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 
 export default function App(){
-  const [inputNum, setInputNum] = useState(0);
-  const [countNum, setCountNum] = useState();
-
-  const res = useMemo(()=>{
-    let sum = 0;
-    for (let i=0; i<=inputNum;i++){
-      sum += i;
-    }
-    return sum;
-  }, [inputNum]);
-
-  
-  const counterInc = ()=>{
-    return setCountNum(res+1)
-  };
-
+  useEffect(()=>{console.log("UE-1")}, []);
+  useEffect(()=>{console.log("UE-2")}, []);
+  useEffect(()=>{ setTimeout(()=>console.log("SeTimeout"),5000) }, []);
+  console.log("After useEffect")
   return(
     <div>
-      <input type="number" 
-      placeholder="Enter Your number to get sum from 1 to n" 
-        value={inputNum}
-        onChange={(e)=>setInputNum(e.target.value)}
-      />
-
-      <p>Sum is: {res}</p>
-      <button onClick={counterInc}>Counter </button>
-      <p>Counter sum is: {countNum}  👈🏽[Sum+1]</p>
+      <h1>Hello {console.log("Hello is rendered...")}</h1>
     </div>
   )
 }
