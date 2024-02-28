f690753fd69662575c0114d871a8c46101118074
pankaj-gitcode <pankajaditya75@gmail.com>
2024-02-28 21:43:32 +0530

    recap_useCallback_with_memo_with_dynamic_prop_getting_functions_as_reference_value

--- revise/code_recap/src/App.jsx
+++ revise/code_recap/src/App.jsx
@@ -1,29 +1,32 @@
 //use of memo(), comparing the result with prop as static value and another one with reference value from the function
 
-import React, {memo, useState} from 'react'
+import React, {memo, useState, useCallback} from 'react'
 import './App.css'
 
 export default function App(){
   const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
 
   //button function to re-render the counter and the components
   const counterFunc = ()=>{
-    console.log("Button re-rendering")
+    console.log("Button-1 re-rendering as well as Display-2 ")
     setCounter(counter=>counter+1)
   };
+  const counter2Func = ()=>{console.log("Button-2 re-rendering but Display-2 not"); setCounter2(counter2=>counter2+1)}
 
   //For Display1&2 components
   const res = 100;
-  const display2Val = ()=>{
+  const display2Val = useCallback(()=>{ //used useCallback, now Display-2 also not re-render unless dependency vary
     const val = 200;
     return val;
-  }
+  }, [counter])
 
   return(
     <div>
       <button onClick={counterFunc}>Counter {counter}</button>
       <Display1 result = {res}/>
       <Display2 result= {display2Val} />
+      <button onClick={counter2Func}>click & check if Display-2 is re-rendering or not</button>
     </div>
   )
 }
