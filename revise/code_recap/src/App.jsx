a227aaaffc14aed173755c7227d4f748a0b01ae8
pankaj-gitcode <pankajaditya75@gmail.com>
2024-02-29 17:34:29 +0530

    recap_useRef()_v1

--- revise/code_recap/src/App.jsx
+++ revise/code_recap/src/App.jsx
@@ -1,21 +1,31 @@
 // how many times the page loads there should be Page-count displayed 
-let pageCount = 0;
-import React, { useState } from 'react'
+// if we don't remove strictMode in main.jsx then we've re-rendering 2 times where pageCount=2 [initial value]
+// whereas if we remove React.StrictMode then we have pageCount=1 [initial value]
+// means due to React.StrictMode we have one extra rendering
+
+//but we can't remove Strict mode everytime, so we'll use 'useRef()'
+
+import React, {useState, useRef} from 'react'
 import './App.css'
+
+let pageRender = 0;
 
 export default function App(){
   const [count, setCount] = useState(0);
-
+  const componentRender = useRef(0);
+ console.log(JSON.stringify(componentRender))
   const counterFunc = ()=>{
     setCount(count=>count+1);
   }
 
-  pageCount += 1;
+  pageRender += 1; // with Strict Mode, so initial value=2
+  componentRender.current += 1;  //with StrictMode, but used "useRef" so intial value=1
 
   return(
     <div>
-      <button onClick={counterFunc}>Count {count}</button>
-      <p>Page Count: {pageCount}</p>
+      <button onClick={counterFunc}>Counter {count}</button>
+      <p>Page Render {pageRender} times</p>
+      <p>Component Rendered {componentRender.current} times</p>{console.log(JSON.stringify(componentRender))}
     </div>
   )
 }
