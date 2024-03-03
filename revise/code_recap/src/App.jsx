a920bd58e205dc4e5d5c991d527ead025f376387
pankaj-gitcode <pankajaditya75@gmail.com>
2024-03-03 22:06:46 +0530

    recap_before_navigation

--- revise/code_recap/src/App.jsx
+++ revise/code_recap/src/App.jsx
@@ -1,31 +1,18 @@
-import React from 'react'
+import React from "react";
 import './App.css'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import { Landing } from './component/Landing'
-import { Dashboard } from './component/Dashboard'
-import { Home } from './component/Home'
-
+import {BrowserRouter, Route, Router} from 'react-router-dom'
+import { Landing } from "./component/Landing";
+import { Home } from "./component/Home";
 
 export default function App(){
-
-  const landing = ()=>{window.location.href='/landing'};
-  const dashboard = ()=>{window.location.href='/dashboard'}
-
   return(
     <div>
-      <div style={{backgroundColor:'brown'}}><center>Header</center>
-      <button onClick={landing}>Landing Page</button>
-      <button onClick={dashboard}>Dashboard Page</button>
-      </div>
       <BrowserRouter>
-        <Routes>
-          <Route path={'/landing'}  element={<Landing/>}/>
-          <Route path={'/dashboard'} element={<Dashboard/>}/>
-          <Route path={'/'} element={<Home/>}/>
-        </Routes>
+        <Router>
+          <Route path="/" element={<Home />} />
+          <Route path="/landing" element={Landing} />
+        </Router>
       </BrowserRouter>
-      <div style={{backgroundColor:'brown'}}><center>Header</center></div>
-
     </div>
   )
 }
