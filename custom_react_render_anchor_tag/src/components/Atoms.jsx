0a4cbb5024aff6f876f2e8a322401cc5efa5383d
pankaj-gitcode <123591656+pankaj-gitcode@users.noreply.github.com>
2024-03-26 22:37:39 +0530

    Update Atoms.jsx

--- custom_react_render_anchor_tag/src/components/Atoms.jsx
+++ custom_react_render_anchor_tag/src/components/Atoms.jsx
@@ -16,6 +16,7 @@ export const wordListAtom = atomFamily({
     default: selectorFamily({
         key: 'wordListSelector',
         get:(id)=>async ({get})=>{
+            if(id<=0){return id=1}
             const response = await axios.get(`https://random-word-api.herokuapp.com/word?number=${id}`);
             console.log(`ATOM: ${id}, ${response.data}`)
             return response.data;
