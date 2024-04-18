5c6489312f314a445043842ccea151c3716d5308
pankaj-gitcode <123591656+pankaj-gitcode@users.noreply.github.com>
2024-04-19 01:24:09 +0530

    Update debounceAtom.jsx

--- debounceAtom.jsx
+++ debounceAtom.jsx
@@ -1,41 +1,48 @@
-import {atom, atomFamily, selectorFamily} from 'recoil'
+import React, { useEffect } from 'react'
+import '../App.css'
+import GitInfo from './GitInfo'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { fetchAPIDataAtom, inputAtom } from './Atom'
 import axios from 'axios'
 
+export default function MainApp(){
+    const inputValue = useRecoilValue(inputAtom);
+    const [fetchAPIData, setFetchAPIData] = useRecoilState(fetchAPIDataAtom);
 
-export const inputSearchAtom = atom({
-    key: 'inputSearchAtom',
-    default: "raj"
-})
+    useEffect(()=>{
+        
+        const getData = setTimeout(()=>{
+            axios.get(`https://api.github.com/users/${inputValue}`)
+            .then(response=>response.data)
+            .then(data=>{console.log("FetchAPI:", data);setFetchAPIData(data)})
+        }, 800)
 
+        return ()=>clearTimeout(getData)
+        
 
-export const gitAPIAtom = atomFamily({
-    key: 'gitAPIAtom',
-    default: selectorFamily({
-        key: 'gitAPISelector',
-        get:(id)=>({get})=>{
-            var apiValue;
-            const apiVal = setTimeout(async(apiValue)=>{
-
-                const response = await axios.get(`https://api.github.com/users/${id}`)
-                apiValue = response.data;
-                console.log('APIVALUE:', apiValue)
-                return apiValue;
-            },1000)
-            console.log('apiVal::' , typeof apiVal, apiValue)
-            return apiValue;
-        }
-    })
-})
+    }, [inputValue])
 
 
+    return(
+        <div>
+            <GitInfo/>
+            <Display/>
+        </div>
+    )
+}
 
-export const debounce = (fn)=>{
-    let timeoutID;
-    return function(){
-        clearTimeout(timeoutID);
 
-        timeoutID = setTimeout(()=>{
-            fn
-        }, 800);
-    }
+
+const Display = ()=>{
+        const apiData = useRecoilValue(fetchAPIDataAtom)
+    return(
+        <div>
+            <ul>
+                <li>Login: {apiData.login}</li>
+                <li>ID: {apiData.id}</li>
+                <li>Image: {apiData.avatar_url}</li>
+                <li><img src={apiData.avatar_url} alt="GIT User DP" /></li>
+            </ul>
+        </div>
+    )
 }
