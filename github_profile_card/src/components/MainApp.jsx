d96ce592e23c63ea7dbb71282ef24db2834e58e9
pankaj-gitcode <pankajaditya75@gmail.com>
2024-04-19 17:43:06 +0530

    feat added debounce input button tag

--- github_profile_card/src/components/MainApp.jsx
+++ github_profile_card/src/components/MainApp.jsx
@@ -1,44 +1,80 @@
-import React, {usecallback} from 'react'
+import React, { useEffect } from 'react'
 import '../App.css'
 import { useRecoilState, useRecoilValue } from 'recoil'
-import { debounceAtom, gitAPIAtom, inputSearchAtom } from './Atom'
-import GitInfo from './GitInfo'
-// import Debounce from './Debounce'
+import { btnAtom, fetchAPIDataAtom, inputAtom } from './Atom'
+import axios from 'axios';
+import GitInfo from './GitInfo';
 
 export default function MainApp(){
-    const inputSearchData = useRecoilValue(inputSearchAtom)
-    const debouncedFunc = useRecoilValue(gitAPIAtom);
+    const inputValue = useRecoilValue(inputAtom);
 
-    
+    //**uncomment below if don't want to call API from button, make sure to comment button API call**
+    // const [fetchDataFromAPI, setFetchDataFromAPI] = useRecoilState(fetchAPIDataAtom);
+
+    // useEffect(()=>{
+    //     const getData = setTimeout(()=>{
+    //         axios.get(`https://api.github.com/users/${inputValue}`)
+    //         .then(response=>response.data)
+    //         .then(data=>{console.log("API DATA:",data); setFetchDataFromAPI(data)})
+    //     }, 1000)
+    //     return ()=>clearTimeout(getData);
+
+    // }, [inputValue])
+
     return(
         <div>
-            <InputData/>
-            <GitInfo id={debouncedFunc?.login}/>
-            {/* <Debounce func={gitAPIData} delay={3000}/> */}
+            <GitInfo/>
+            <Button/>
+            <Display/>
         </div>
     )
 }
 
-//input element
-const InputData = ()=>{
-    const [inputSearchData, setInputSearchData] = useRecoilState(inputSearchAtom);
-    const [debounceFunc] = useRecoilState();
-    
-    const debouncedFunc = useRecoilValue( usecallback(debounceFunc(gitAPIAtom, 300),[inputSearchData, gitAPIAtom]) )
-    
-    
-    const handleDebounce = (e)=>{
-        setInputSearchData(e.target.value);
-        
+const Button = ()=>{
+    const inputValue = useRecoilValue(inputAtom);
+    const [fetchDataFromAPI, setFetchDataFromAPI] = useRecoilState(fetchAPIDataAtom);
+    const [btnAPI, setBtnAPI] = useRecoilState(btnAtom);
+
+    //API calling, fetch API Atom called and 'API data' used as an argument
+    useEffect(()=>{
+
+        const getData = setTimeout(()=>{
+            axios.get(`https://api.github.com/users/${inputValue}`)
+            .then(response=>response.data)
+            .then(data=>{console.log(`BUTTON: ${data}`); setFetchDataFromAPI(data)})
+        }, 10000)
+
+        return ()=>clearTimeout(getData);
+
+    }, [inputValue])
+
+    //once button clicked, setBtnAPI() invoked [fetchDataFromAPI used an argument]
+    const clickHandle = ()=>{
+        const getData = setTimeout(()=>{
+            setBtnAPI(fetchDataFromAPI)
+        }, [2000])
     }
 
     return(
         <div>
-            <input type="text"
-            value={inputSearchData}
-            onChange={handleDebounce}
-             />
+            <button onClick={clickHandle}>Click</button>
         </div>
     )
 }
 
+const Display = ()=>{
+    // const fetchAPIData = useRecoilValue(fetchAPIDataAtom);
+    const fetchAPIData = useRecoilValue(btnAtom);
+    
+    
+    return(
+        <div>
+            <ul>
+                <li>Login: {fetchAPIData.login}</li>
+                <li>ID: {fetchAPIData.id}</li>
+                <li>DP: {fetchAPIData.avatar_url}</li>
+                <li><img src={fetchAPIData.avatar_url} alt="" /></li>
+            </ul>
+        </div>
+    )
+}
